9aed205b7055317f9ad76bd1d3921cf6f958604b
Adrià Munuera Borrell <adria.munuera@travelperk.com>
2020-02-19 14:34:40 +0100

    Added changes regarding users, superusers and admin of the core app

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,23 @@
-# from django.contrib import admin
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext as localize
 
-# Register your models here.
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (localize('Personal info'), {'fields': ('name',)}),
+        (
+            localize('Permissions'),
+            {
+                'fields': ('is_active', 'is_staff', 'is_superuser')
+            }
+        ),
+        (localize('Important dates'), {'fields': ('last_login',)})
+    )
+
+admin.site.register(models.User,  UserAdmin)
