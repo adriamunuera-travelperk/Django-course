6ae0207cb44e1b8777ec51ab8c46e8362a1a1c92
Adrià Munuera Borrell <adria.munuera@travelperk.com>
2020-02-19 16:20:05 +0100

    Configured POSTGRES DB

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,11 @@ MAINTAINER Adrià Munuera
 
 ENV PYTHONUNBUFFERED 1
 COPY ./requeriments.txt /requeriments.txt
+RUN apk add --update --no-cache postgresql-client
+RUN apk add --update --no-cache --virtual .tmp-build-deps \
+     gcc libc-dev linux-headers postgresql-dev
 RUN pip install -r /requeriments.txt
+RUN apk del .tmp-build-deps
 RUN mkdir /app
 WORKDIR /app
 COPY ./app /app
--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -9,12 +9,12 @@ class Command(BaseCommand):
     """Django command to pause execution until database is avaliable"""
 
     def handle(self, *args, **options):
-        self.stdout.write('Waiting for database...')
+        self.stdout.write('Waiting for DB...')
         db_conn = None
         while not db_conn:
             try:
                 db_conn = connections['default']
             except OperationalError:
-                self.stdout.write('Database unavaliable, waiting for 1 second...')
+                self.stdout.write('DB unavaliable, waiting for 1 second...')
                 time.sleep(1)
         self.stdout.write(self.style.SUCCESS('Database available!'))
