fb8a6bd35c788d2257ce32baab0d9da3ff5033ce
Adrià Munuera Borrell <adria.munuera@travelperk.com>
2020-02-20 11:13:57 +0100

    Added user modification page

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -8,6 +8,7 @@ from rest_framework import status
 
 CREATE_USER_URL = reverse('user:create')
 TOKEN_URL = reverse('user:token')
+LOGGED_URSER_URL = reverse('user:logged_user')
 
 
 def create_user(**params):
@@ -96,3 +97,47 @@ class PublicUserAPITests(TestCase):
         )
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_retrieve_user_unauthorized(self):
+        """ Test that authentication is required for users"""
+        res = self.client.get(LOGGED_URSER_URL)
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class PrivateUserAPITests(TestCase):
+    """ API tests that require authentication"""
+
+    def setUp(self):
+        self.user = create_user(
+            email='test@test.com',
+            password='admin',
+            name='John'
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def test_retrieve_profile_success(self):
+        """ Test retrieving profile for logged in user"""
+        response = self.client.get(LOGGED_URSER_URL)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, {
+            'name': self.user.name,
+            'email': self.user.email
+        })
+
+    def test_post_logged_user_not_allowed(self):
+        """ Test that POST requests are not allowed"""
+        response = self.client.post(LOGGED_URSER_URL, {})
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_405_METHOD_NOT_ALLOWED
+        )
+
+    def test_update_user_profile(self):
+        """ Test updating the user profile for authenticated user"""
+        payload = {'name': 'new name', 'password': 'newpassword'}
+        response = self.client.patch(LOGGED_URSER_URL, payload)
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.name, payload.get('name'))
+        self.assertTrue(self.user.check_password(payload.get('password')))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
